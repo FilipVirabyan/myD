e21ade53825e560665930087480e8bd98c5fbc13
Filip Virabyan <filipvirabyan12@gmail.com>
2024-10-04 16:27:57 +0400

    test

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
 // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
-let holding = params.holding || 'ee';
+let holding = params.holding; // "some_value"
 console.log(holding)
 
 if (holding) {
